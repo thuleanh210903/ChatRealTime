75918abc6290feea9548bca5c206c08aa49f478b
ThuLe <thuanhle219@gmail.com>
2025-07-31 14:39:55 +0700

    feat: add background and build html layout

--- src/shared/pages/auth/container/Register.tsx
+++ src/shared/pages/auth/container/Register.tsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+import { Button } from '../components/Button';
+import { Input } from '../components/Input';
+
 const Register = () => {
-  return <div>This is Register Page</div>;
+  return (
+    <div className="page page-register">
+      <h1 className="page-title">REGISTER</h1>
+      <form className="page-form">
+        <Input label="Email" placeHolder="user@gmail.com" />
+        <Input label="Password" type="password" placeHolder="123456" />
+        <Button label="Register" />
+      </form>
+      <p>
+        <Link to="">Already have an account</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
