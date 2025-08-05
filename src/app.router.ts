161e80cf85d118052c7090151285ab89becdbb63
ThuLe <thuanhle219@gmail.com>
2025-08-05 08:40:53 +0700

    feat: define router homepag

--- src/app.router.ts
+++ src/app.router.ts
@@ -1,5 +1,6 @@
 import type { PageRoute } from './core/router/router.interface';
 import authRoutes from './shared/pages/auth/auth.route';
+import homeRoutes from './shared/pages/home/home.route';
 
-const appRouter: PageRoute[] = [...authRoutes];
-export default appRouter
+const appRouter: PageRoute[] = [...authRoutes, ...homeRoutes];
+export default appRouter;
---
+++ src/shared/pages/home/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <>Homepage</>;
+};
+
+export default Home;
