0309d978f4a5fac47d9b34c99c1875faa0553348
ThuLe <thuanhle219@gmail.com>
2025-08-08 08:27:11 +0700

    feat: build header layout

--- src/shared/layouts/Header.tsx
+++ src/shared/layouts/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        <Avatar />
+        <Avatar className="avatar-xl" />
         <div className="header-text">
           <h2 className="header-title">John Doe</h2>
           <p className="header-subtitle">Online</p>
