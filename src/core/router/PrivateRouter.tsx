17df5e9139f9e3b125d233bf13a9b10384bbf113
ThuLe <thuanhle219@gmail.com>
2025-09-15 22:52:38 -0400

    feat: define private router

--- src/core/router/PrivateRouter.tsx
+++ src/core/router/PrivateRouter.tsx
@@ -1,14 +1,9 @@
 import { Navigate } from 'react-router-dom';
 
 import { useUser } from '../../context/UserProvider';
-import { AppRoutes } from '../../shared/constants/app-routes';
 
 export const PrivateRouter = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated } = useUser();
 
-  return isAuthenticated ? (
-    <>{children}</>
-  ) : (
-    <Navigate to={AppRoutes.REGISTER} />
-  );
+  return isAuthenticated ? <>{children}</> : <Navigate to={'/auth/register'} />;
 };
